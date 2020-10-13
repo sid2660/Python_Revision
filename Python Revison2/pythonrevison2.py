01771eb29e55b4d11d4091c5ac77a4763ec3a265
Siddhartha Singh <58968275+sid2660@users.noreply.github.com>
2020-10-13 21:04:30 +0530

    Add files via upload
    Python Revision

---
+++ Python Revison2/pythonrevison2.py
@@ -0,0 +1,40 @@
+### Revision Python Totorials ####
+
+######### Video ---> 5 
+
+# print('Hello World')
+
+######### Video ---> 6(Print function)
+
+# print("Hello World")
+# print('Hello World')
+
+## String --> Collection of character inside "Double quotes" or 'Single Quotes'
+## --> You can use 'Single quotes' inside "Double Quotes"
+##                  OR    
+## --> "Double Quotes " inside 'Single Quotes'
+
+# print("Hello 'world' World")
+# print('Hello "world" World')
+
+############ Video --> 7 (Escape Sequences)
+
+# print("Hello \"World\" world")
+
+##                      !------------------------------------------!
+##                      !  Escape sequences    !  Meaning          !
+##                      !----------------------!-------------------!
+##                      !  \'                  ! Single Quote      !
+##                      !----------------------!-------------------!
+##                      !  \"                  ! Duble Quote       !
+##                      !----------------------!-------------------!
+##                      !  \\                  ! Backslash         !
+##                      !----------------------!-------------------!
+##                      !  \n                  ! New line          !
+##                      !----------------------!-------------------!
+##                      !  \t                  ! Tab               !
+##                      !----------------------!-------------------!
+##                      !  \b                  ! Backspace         !
+##                      !------------------------------------------!
+
+# print('I\'m Siddhartha')
---
+++ winsound.py
@@ -0,0 +1,4 @@
+import winsound
+frequency = 250
+duration = 10000
+winsound.Beep(frequency,duration)
